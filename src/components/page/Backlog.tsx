9c9b8819be86b5761e90a600267b08140c0228c6
sarveshgandhi <42963045+sarveshgandhi@users.noreply.github.com>
2023-05-13 20:37:38 +0530

    adding backlog page

---
+++ src/components/page/Backlog.tsx
@@ -0,0 +1,12 @@
+import './Backlog.css'
+
+const Backlog = () => {
+  return (
+    <div className="backlog-page-head">
+        Backlog component
+        
+    </div>
+  )
+}
+
+export default Backlog
