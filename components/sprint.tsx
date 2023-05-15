7c1cd24e87caaa8b4fa9216f45a574c8ff4fe1dd
monu <sawlanimohnish@gmail.com>
2023-05-15 20:05:46 +0530

    sprint comonent added

--- components/sprint.tsx
+++ components/sprint.tsx
@@ -1,8 +1,36 @@
+import React, { useState } from 'react';
+
 function Sprint() {
-  const items = ["NewYork", "Jenkins", "London", "China", "Great India"];
+  const items = [
+    {
+    id: 'string',
+    isDateSet: 'boolean',
+    startDate: 'string',
+    endDate: 'string',
+    name: 'string',
+    goal: 'string',
+    hasEnded: 'boolean',
+    issues: 'Issue',
+    projectname:'string'
+  }];
+  //issues
+  const [issues, setIssues]=useState(0);
   return (
     <>
-      <div></div>
+      <div className="d-flex  flex-wrap  bg-light p-2">
+
+        <div className="sprintName-date-issues d-flex px-1">
+        <h6 className="SprintName ">Sprint1</h6>
+        <span className="Dates">Add dates</span><br/>
+        <span className="IssuesCount px-1">({issues} issues)</span>
+        </div>
+    <div className="issue d-flex px-1 ">
+      <span className='IssueType'></span>
+      <h6 className="IssueTitle">Issues</h6>
+
+
+    </div>
+      </div>
     </>
   );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import './App.css'
-import List from '../components/sprint'
-
-function App() {
-  
-  return (
-    <>
-      <h1>Hello World</h1>
-      <List/>
-=======
-
+import Sprint from '../components/sprint'
 function App() {
 
   return (
     <>
->>>>>>> 9c9b8819be86b5761e90a600267b08140c0228c6
+    {<Sprint/>}
+
     </>
   )
 }
