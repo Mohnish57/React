808c2b530912cfa5d0163822f07e2a79de870002
sarveshgandhi <42963045+sarveshgandhi@users.noreply.github.com>
2023-05-20 02:07:52 +0530

    button extrated and reused

---
+++ src/components/button/CompleteSprintButton.tsx
@@ -0,0 +1,14 @@
+import "./CompleteSprintButton.css";
+
+function CompleteSprintButton() {
+  return (
+    <button
+      type="button"
+      className="btn ml-auto btn-light border complete-sprint"
+    >
+      Complete sprint
+    </button>
+  );
+}
+
+export default CompleteSprintButton;
